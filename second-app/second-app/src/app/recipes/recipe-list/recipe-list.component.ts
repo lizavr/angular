478d8ded0fbd7fff505400c6cb4236049ddc6125
lizavr <elizavetarabetskaya@gmail.com>
2023-10-16 23:17:06 +0400

    feat: lest of recipes

---
+++ second-app/second-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'a test recipe',
+      'test test test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LAEih9p--b4Jgwk-iXWC7AxpnJYf8TNVSQ&usqp=CAU'
+    ),
+    new Recipe(
+      'a test recipe',
+      'test test test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LAEih9p--b4Jgwk-iXWC7AxpnJYf8TNVSQ&usqp=CAU'
+    ),
+  ];
+  constructor() {}
+  ngOnInit(): void {}
+}
