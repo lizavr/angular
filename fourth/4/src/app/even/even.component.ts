b408e4f1f8be83cdcb67be66293c2f18b98fad31
lizavr <elizavetarabetskaya@gmail.com>
2023-10-19 15:12:26 +0400

    4

---
+++ fourth/4/src/app/even/even.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-even',
+  templateUrl: './even.component.html',
+  styleUrls: ['./even.component.css'],
+})
+export class EvenComponent implements OnInit {
+  ngOnInit(): void {}
+  @Input() number: number = 0;
+}
