d196476b870192f5402b9189fb083473d95a71c2
lizavr <elizavetarabetskaya@gmail.com>
2023-10-22 22:17:24 +0400

    service

--- 5/src/app/new-account/new-account.component.ts
+++ 5/src/app/new-account/new-account.component.ts
@@ -6,13 +6,17 @@ import { AccountService } from '../account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountService],
+  providers: [LoggingService],
 })
 export class NewAccountComponent {
   constructor(
     private loggingService: LoggingService,
     private accountService: AccountService
-  ) {}
+  ) {
+    this.accountService.statusUpdated.subscribe((status: string) => {
+      alert(status);
+    });
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
