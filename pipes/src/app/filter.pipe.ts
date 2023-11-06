4c59ea417a1c35f3e1567eeefe7728452366a93c
lizavr <elizavetarabetskaya@gmail.com>
2023-11-06 17:34:50 +0400

    pipes

---
+++ pipes/src/app/filter.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterString: string, propName: string): any {
+    console.log('value:' + value);
+    console.log('filterString:' + filterString);
+
+    console.log('propName:' + propName);
+
+
+    if (value.length === 0 || filterString === '') {
+      return value;
+    }
+    const resultArray = [];
+    for (const item of value) {
+      if (item[propName] === filterString) {
+        resultArray.push(item);
+      }
+    }
+    return resultArray;
+  }
+}
